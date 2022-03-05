e2d52795f028073e8280685f5db124bd8de08628
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-06 02:33:39 +0500

    any differ

---
+++ src/redux/reducers/news.js
@@ -0,0 +1,39 @@
+const initialState = {
+    news:[],
+    newsLoadingStatus: 'sam',
+}
+
+const news = (state = initialState, action) => {
+    switch(action.type){
+        case "NEWS_FETCHING":
+            return{
+                ...state,
+                newsLoadingStatus:'loading'
+            }
+        case "NEWS_FETCHED":
+            return{
+                ...state,
+                news: action.payload,
+                newsLoadingStatus: 'sam'
+            }
+        case "NEWS_FETCHING_ERROR":
+            return{
+                ...state,
+                newsLoadingStatus: 'error'
+            }
+        case "NEWS_CREATED":
+            return{
+                ...state,
+                news: [...state.news, action.payload],
+            }
+        case "NEWS_DELETED":
+            return{
+                ...state,
+                news: state.news.filter(s => s.id !== action.payload),
+            }
+        default:
+            return state;
+    }
+}
+
+export default news;
