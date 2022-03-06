a2e43644a6e8d42a15a6e6ec284ab70566bf976c
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-06 23:24:33 +0500

    Middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {createStore, combineReducers, compose} from 'redux';
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
 import App from './components/App';
 import news from './redux/reducers/news';
 import filter from './redux/reducers/filter';
 import './index.scss'
+
+const stringMiddleware =() => (next) => (action) => {
+  if(typeof action === "string"){
+    return next({type: action});
+  }
+  return next(action);
+}
 
 const enhancer = (createStore) => (...args) => {
   const store = createStore(...args);
@@ -20,7 +27,7 @@ const enhancer = (createStore) => (...args) => {
 
   return store;
 }
-const store = createStore(combineReducers({news, filter}), compose( enhancer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(combineReducers({news, filter}), compose(applyMiddleware(stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
