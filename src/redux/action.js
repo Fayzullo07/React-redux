c31128e1c5238b074a09fc9244549ee9bbe1f810
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-07 22:05:17 +0500

    createReducer imorted @reduxjs/toolkit

--- src/redux/action.js
+++ src/redux/action.js
@@ -7,11 +7,11 @@ export const fetchNews = (request) => (dispatch) => {
         .catch(() => dispatch(newsFetchingError()))
 }
 export const newsFetching = createAction("NEWS_FETCHING");
-export const newsFetched = (news) => ({type:"NEWS_FETCHED", payload: news});
-export const newsFetchingError = () => ({type: "NEWS_FETCHING_ERROR"});
-export const newsCreated = (news) => ({type: "NEWS_CREATED", payload: news});
-export const filtersFetching = () => ({type: "FILTERS_FETCHING"});
-export const filtersFetched = (filters) => ({type: "FILTERS_FETCHED", payload: filters});
-export const filtersFetchingError = () => ({type: "FILTERS_FETCHING_ERROR"}); 
+export const newsFetched = createAction("NEWS_FETCHED");
+export const newsFetchingError = createAction("NEWS_FETCHING_ERROR");
+export const newsCreated = createAction("NEWS_CREATED");
+export const filtersFetching = createAction("FILTERS_FETCHING");
+export const filtersFetched = createAction("FILTERS_FETCHED");
+export const filtersFetchingError = createAction("FILTERS_FETCHING_ERROR"); 
 export const activeFilterChanged = (filter) => (dispatch) => (setTimeout(() => (dispatch({type: "ACTIVE_FILTER_CHANCHED", payload: filter})), 500));
-export const newsDeleted = (id) => ({type: "NEWS_DELETED", payload: id});
+export const newsDeleted = createAction("NEWS_DELETED");
