e07a57083f3db36371837e31923137a55aa67e44
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-07 20:50:35 +0500

    Middlware configureStore 2

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+// import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import {configureStore} from '@reduxjs/toolkit'
+// import ReduxThunk from 'redux-thunk';
+import news from './reducers/news';
+import filter from './reducers/filter';
+import { stringMiddleware } from '../middleware/stringMiddleware';
+
+  
+// export const store = createStore(combineReducers({news, filter}), compose(applyMiddleware(ReduxThunk, stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+export const store = configureStore({
+    reducer: {news, filter},
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production'
+})
