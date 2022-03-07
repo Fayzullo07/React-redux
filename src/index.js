218df324e461461c345e6ae9804aced263ebbaf7
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-07 20:49:37 +0500

    Middlware configureStore

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
-import ReduxThunk from 'redux-thunk';
+import {store} from './redux/store'
 import App from './components/App';
-import news from './redux/reducers/news';
-import filter from './redux/reducers/filter';
 import './index.scss'
 
-const stringMiddleware =() => (next) => (action) => {
-  if(typeof action === "string"){
-    return next({type: action});
-  }
-  return next(action);
-}
-
-const store = createStore(combineReducers({news, filter}), compose(applyMiddleware(ReduxThunk, stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
--- src/redux/action.js
+++ src/redux/action.js
@@ -2,7 +2,7 @@ export const fetchNews = (request) => (dispatch) => {
     dispatch(newsFetching());
     request("http://localhost:3001/news")
         .then(data => dispatch(newsFetched(data)))
-    .   catch(() => dispatch(newsFetchingError()))
+        .catch(() => dispatch(newsFetchingError()))
 }
 export const newsFetching = () => ({type: "NEWS_FETCHING"});
 export const newsFetched = (news) => ({type:"NEWS_FETCHED", payload: news});
@@ -11,5 +11,5 @@ export const newsCreated = (news) => ({type: "NEWS_CREATED", payload: news});
 export const filtersFetching = () => ({type: "FILTERS_FETCHING"});
 export const filtersFetched = (filters) => ({type: "FILTERS_FETCHED", payload: filters});
 export const filtersFetchingError = () => ({type: "FILTERS_FETCHING_ERROR"}); 
-export const activeFilterChanged = (filter) => (dispatch) => (setTimeout(() => (dispatch({type: "ACTIVE_FILTER_CHANCHED", payload: filter})), 1000));
+export const activeFilterChanged = (filter) => (dispatch) => (setTimeout(() => (dispatch({type: "ACTIVE_FILTER_CHANCHED", payload: filter})), 500));
 export const newsDeleted = (id) => ({type: "NEWS_DELETED", payload: id});
